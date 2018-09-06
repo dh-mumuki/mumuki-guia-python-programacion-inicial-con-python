c71589f6abdf132fbf32b3bb5918566f5303be3e
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-06 12:57:30 -0400

    Mumuki Export on 2018-09-06 12:57:30 -0400

--- 00006_Strings - Concat/test.py
+++ 00006_Strings - Concat/test.py
@@ -1,3 +1,3 @@
 class TestFixtures(unittest.TestCase):
    def test_concat(self):
-      self.assertEquals(concat_lista([1, 5, 12, 2]), str(15122))
+      self.assertTrue(concat_lista([1, 5, 12, 2]) == str(15122))
