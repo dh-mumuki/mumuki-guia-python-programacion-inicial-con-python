09aafb18b43b083d8715ea5699065e4c575b0061
andres@digitalhouse.com <andres@digitalhouse.com>
2018-10-03 21:09:18 -0400

    Mumuki Export on 2018-10-03 21:09:18 -0400

--- 00013_New exercise/test.py
+++ 00013_New exercise/test.py
@@ -1,3 +1,5 @@
-class test(unittest):
-  def test_1():
-    return a
+class TestFixtures(unittest.TestCase):
+  def test_impares(self):
+    self.assertEquals(impares(8), [1,3,5,7])
+  def test_iimpares(self):
+    self.assertEquals(impares(9), [1,3,5,7])
