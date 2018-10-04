5edb745afd23207d59b68cde133a0638c0e8320b
andres@digitalhouse.com <andres@digitalhouse.com>
2018-10-03 21:41:08 -0400

    Mumuki Export on 2018-10-03 21:41:08 -0400

--- 00005_Check/test.py
+++ 00005_Check/test.py
@@ -1,6 +1,6 @@
 class TestFixtures(unittest.TestCase):
   def test_True(self):
-    self.assertTrue(5,estar([1, 5, 8, 3]))
+    self.assertTrue(estar(5,[1, 5, 8, 3]))
 
-#  def test_False(self):
- #   self.assertFalse(estar([1, 5, 8, 3], 2))
+  def test_False(self):
+    self.assertFalse(estar( 2, [1, 5, 8, 3]))
